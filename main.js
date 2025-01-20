311c7487903466d93d2c0c01c8c91edba966f5a9
DenisPadalko <DenisPadalko@users.noreply.github.com>
2025-01-20 09:15:44 +0200

    completed task

--- main.js
+++ main.js
@@ -21,15 +21,30 @@ function Model(name) {
     this.vIndexBuffer = gl.createBuffer();
     this.uNormalBuffer = gl.createBuffer();
     this.vNormalBuffer = gl.createBuffer();
+    this.uTexCoordBuffer = gl.createBuffer();
+    this.vTexCoordBuffer = gl.createBuffer();
+    this.uTangentBuffer = gl.createBuffer();
+    this.vTangentBuffer = gl.createBuffer();
+    this.uBitangentBuffer = gl.createBuffer();
+    this.vBitangentBuffer = gl.createBuffer();
     this.uIndexCount = 0;
     this.vIndexCount = 0;
 
-    this.BufferData = function(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals) {
+    this.BufferData = function(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.uVertexBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uVertices), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.uNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uNormals), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uTexCoordBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uTexCoords), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uTangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uTangents), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uBitangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uBitangents), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.uIndexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(uIndices), gl.STATIC_DRAW);
@@ -41,6 +56,15 @@ function Model(name) {
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vNormalBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vNormals), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vTexCoordBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vTexCoords), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vTangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vTangents), gl.STATIC_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vBitangentBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vBitangents), gl.STATIC_DRAW);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vIndexBuffer);
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(vIndices), gl.STATIC_DRAW);
@@ -57,6 +81,18 @@ function Model(name) {
         gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uTexCoordBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTexCoords, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTexCoords);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uTangentBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTangent);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.uBitangentBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribBitangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribBitangent);
+
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.uIndexBuffer);
         gl.drawElements(gl.TRIANGLES, this.uIndexCount, gl.UNSIGNED_SHORT, 0);
 
@@ -67,6 +103,18 @@ function Model(name) {
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vNormalBuffer);
         gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribNormal);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vTexCoordBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTexCoords, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTexCoords);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vTangentBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribTangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribTangent);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vBitangentBuffer);
+        gl.vertexAttribPointer(shProgram.iAttribBitangent, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribBitangent);
 
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.vIndexBuffer);
         gl.drawElements(gl.TRIANGLES, this.vIndexCount, gl.UNSIGNED_SHORT, 0);
@@ -79,6 +127,9 @@ function ShaderProgram(name, program) {
 
     this.iAttribVertex = -1;
     this.iAttribNormal = -1;
+    this.iAttribTexCoords = -1;
+    this.iAttribTangent = -1;
+    this.iAttribBitangent = -1;
     this.iModelViewProjectionMatrix = -1;
     this.iNormalMatrix = -1;
     this.iLightPosition = -1;
@@ -86,6 +137,9 @@ function ShaderProgram(name, program) {
     this.iDiffuseColor = -1;
     this.iSpecularColor = -1;
     this.iShininess = -1;
+    this.iTMU0 = -1;
+    this.iTMU1 = -1;
+    this.iTMU2 = -1;
 
     this.Use = function() {
         gl.useProgram(this.prog);
@@ -115,6 +169,18 @@ function draw() {
     lightAngle += 0.01;
     let lightPosition = [10 * Math.cos(lightAngle), 5, 10 * Math.sin(lightAngle)];
     gl.uniform3fv(shProgram.iLightPosition, lightPosition);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureDiffuse);
+    gl.uniform1i(shProgram.iTMU0, 0);
+
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureSpecular);
+    gl.uniform1i(shProgram.iTMU1, 1);
+
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, surface.idTextureNormal);
+    gl.uniform1i(shProgram.iTMU2, 2);
 
     surface.Draw();
 }
@@ -172,6 +238,66 @@ function calculateNormals(vertices, indices) {
     return normals.flat();
 }
 
+function calculateTangentsAndBitangents(vertices, texCoords, indices) {
+    const tangents = new Array(vertices.length).fill(0);
+    const bitangents = new Array(vertices.length).fill(0);
+
+    for (let i = 0; i < indices.length; i += 3) {
+        const i0 = indices[i] * 3;
+        const i1 = indices[i + 1] * 3;
+        const i2 = indices[i + 2] * 3;
+
+        const v0 = vertices.slice(i0, i0 + 3);
+        const v1 = vertices.slice(i1, i1 + 3);
+        const v2 = vertices.slice(i2, i2 + 3);
+
+        const uv0 = texCoords.slice(indices[i] * 2, indices[i] * 2 + 2);
+        const uv1 = texCoords.slice(indices[i + 1] * 2, indices[i + 1] * 2 + 2);
+        const uv2 = texCoords.slice(indices[i + 2] * 2, indices[i + 2] * 2 + 2);
+
+        const deltaPos1 = [v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2]];
+        const deltaPos2 = [v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2]];
+
+        const deltaUV1 = [uv1[0] - uv0[0], uv1[1] - uv0[1]];
+        const deltaUV2 = [uv2[0] - uv0[0], uv2[1] - uv0[1]];
+
+        const r = 1.0 / (deltaUV1[0] * deltaUV2[1] - deltaUV1[1] * deltaUV2[0]);
+        const tangent = [
+            (deltaPos1[0] * deltaUV2[1] - deltaPos2[0] * deltaUV1[1]) * r,
+            (deltaPos1[1] * deltaUV2[1] - deltaPos2[1] * deltaUV1[1]) * r,
+            (deltaPos1[2] * deltaUV2[1] - deltaPos2[2] * deltaUV1[1]) * r
+        ];
+
+        const bitangent = [
+            (deltaPos2[0] * deltaUV1[0] - deltaPos1[0] * deltaUV2[0]) * r,
+            (deltaPos2[1] * deltaUV1[0] - deltaPos1[1] * deltaUV2[0]) * r,
+            (deltaPos2[2] * deltaUV1[0] - deltaPos1[2] * deltaUV2[0]) * r
+        ];
+
+        tangents[i0] += tangent[0];
+        tangents[i0 + 1] += tangent[1];
+        tangents[i0 + 2] += tangent[2];
+        tangents[i1] += tangent[0];
+        tangents[i1 + 1] += tangent[1];
+        tangents[i1 + 2] += tangent[2];
+        tangents[i2] += tangent[0];
+        tangents[i2 + 1] += tangent[1];
+        tangents[i2 + 2] += tangent[2];
+
+        bitangents[i0] += bitangent[0];
+        bitangents[i0 + 1] += bitangent[1];
+        bitangents[i0 + 2] += bitangent[2];
+        bitangents[i1] += bitangent[0];
+        bitangents[i1 + 1] += bitangent[1];
+        bitangents[i1 + 2] += bitangent[2];
+        bitangents[i2] += bitangent[0];
+        bitangents[i2 + 1] += bitangent[1];
+        bitangents[i2 + 2] += bitangent[2];
+    }
+
+    return { tangents, bitangents };
+}
+
 function CreateSurfaceData() {
     let uVertices = [];
     let vVertices = [];
@@ -179,6 +305,12 @@ function CreateSurfaceData() {
     let vIndices = [];
     let uNormals = [];
     let vNormals = [];
+    let uTexCoords = [];
+    let vTexCoords = [];
+    let uTangents = [];
+    let vTangents = [];
+    let uBitangents = [];
+    let vBitangents = [];
     
     const scale = 0.5;
     const a = 1.5 * scale, b = 3.0 * scale, c = 2.0 * scale, d = 2.0 * scale;
@@ -203,6 +335,7 @@ function CreateSurfaceData() {
             const z = 0.5 * (fv - (d ** 2 - c ** 2) / fv) * sinT;
 
             uVertices.push(x, y, z);
+            uTexCoords.push(i / uSegments, j / vSegments);
         }
     }
 
@@ -222,6 +355,7 @@ function CreateSurfaceData() {
             const z = 0.5 * (fv - (d ** 2 - c ** 2) / fv) * sinT;
 
             vVertices.push(x, y, z);
+            vTexCoords.push(i / uSegments, j / vSegments);
         }
     }
 
@@ -255,7 +389,16 @@ function CreateSurfaceData() {
     uNormals = calculateNormals(uVertices, uIndices);
     vNormals = calculateNormals(vVertices, vIndices);
 
-    return { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals };
+    const uTangentBitangent = calculateTangentsAndBitangents(uVertices, uTexCoords, uIndices);
+    uTangents = uTangentBitangent.tangents;
+    uBitangents = uTangentBitangent.bitangents;
+
+    const vTangentBitangent = calculateTangentsAndBitangents(vVertices, vTexCoords, vIndices);
+    vTangents = vTangentBitangent.tangents;
+    vBitangents = vTangentBitangent.bitangents;
+
+    return { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents };
+
 }
 
 function initGL() {
@@ -266,6 +409,9 @@ function initGL() {
 
     shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
     shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
+    shProgram.iAttribTexCoords = gl.getAttribLocation(prog, "texCoord");
+    shProgram.iAttribTangent = gl.getAttribLocation(prog, "tangent");
+    shProgram.iAttribBitangent = gl.getAttribLocation(prog, "bitangent");
     shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(prog, "ModelViewProjectionMatrix");
     shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
     shProgram.iLightPosition = gl.getUniformLocation(prog, "LightPosition");
@@ -273,6 +419,9 @@ function initGL() {
     shProgram.iDiffuseColor = gl.getUniformLocation(prog, "DiffuseColor");
     shProgram.iSpecularColor = gl.getUniformLocation(prog, "SpecularColor");
     shProgram.iShininess = gl.getUniformLocation(prog, "Shininess");
+    shProgram.iTMU0 = gl.getUniformLocation(prog, "iTMU0");
+    shProgram.iTMU1 = gl.getUniformLocation(prog, "iTMU1");
+    shProgram.iTMU2 = gl.getUniformLocation(prog, "iTMU2");
 
     shProgram.Use();
     gl.uniform4fv(shProgram.iAmbientColor, [0.2, 0.2, 0.2, 1.0]);
@@ -281,8 +430,12 @@ function initGL() {
     gl.uniform1f(shProgram.iShininess, 50.0);
 
     surface = new Model('Surface');
-    const { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals } = CreateSurfaceData();
-    surface.BufferData(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals);
+    const { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents } = CreateSurfaceData();
+    surface.BufferData(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents);
+
+    surface.idTextureDiffuse = LoadTexture("textures/texture_diffuse.png");
+    surface.idTextureSpecular = LoadTexture("textures/texture_specular.png");
+    surface.idTextureNormal = LoadTexture("textures/texture_normal.png");
 
     gl.enable(gl.DEPTH_TEST);
 }
@@ -317,8 +470,8 @@ function updateGranularity() {
     uSegments = parseInt(uSlider.value, 10);
     vSegments = parseInt(vSlider.value, 10);
     
-    const { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals } = CreateSurfaceData();
-    surface.BufferData(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals);
+    const { uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents } = CreateSurfaceData();
+    surface.BufferData(uVertices, vVertices, uIndices, vIndices, uNormals, vNormals, uTexCoords, vTexCoords, uTangents, vTangents, uBitangents, vBitangents);
     draw();
 }
 
